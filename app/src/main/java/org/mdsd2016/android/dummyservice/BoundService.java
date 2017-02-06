2aa7736e9fcbfce783678b9d77d262c965517718
Paulina Berger <paulinaberger@a8ora00.aules.local>
2017-02-06 10:46:46 +0100

    added onClick function to the Display Info button and defined the TextView  - also added to change the TV when buttons are clicked

---
+++ app/src/main/java/org/mdsd2016/android/dummyservice/BoundService.java
@@ -0,0 +1,16 @@
+package org.mdsd2016.android.dummyservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class BoundService extends Service {
+    public BoundService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
